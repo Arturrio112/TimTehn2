4aecfb19813d77aca13ce0cc523b2201d062b8cd
Arturs <arturspalamarcuks@inbox.lv>
2022-06-26 20:47:53 +0300

    added login and signup routes

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -2,5 +2,11 @@ module.exports = {
 
     getHomePage: (req, res) => {
         res.render('main')
+    },
+    getLogIN: (req, res) => {
+        res.render('login')
+    },
+    getSignUP: (req, res) => {
+        res.render('signUp')
     }
 }
--- routes/default.js
+++ routes/default.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const controller = require('../controllers/defaultController')
 
 router.get('/', controller.getHomePage)
-
+router.get('/login', controller.getLogIN)
+router.get('/signup', controller.getSignUP)
 module.exports = router
