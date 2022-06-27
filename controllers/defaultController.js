bed864c431d9bba4e8c730e41ec59e7824d6b96b
Arturs <arturspalamarcuks@inbox.lv>
2022-06-27 13:41:06 +0300

    added routes to views/ fixed some bugs in views

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -8,5 +8,17 @@ module.exports = {
     },
     getSignUP: (req, res) => {
         res.render('signUp')
+    },
+    getAuctionHouse: (req, res) => {
+        res.render('auctionhouse')
+    },
+    getProfile: (req, res) => {
+        res.render('profile')
+    },
+    getItem: (req, res) => {
+        res.render('item')
+    },
+    getAuctionItem: (req, res) => {
+        res.render('itemAuction')
     }
 }
--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express')
+//const mysql = require('mysql')
 const app = express()
 
 const defaultRoute = require('./routes/default')
+/*const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234'
 
+});
+connection.connect((err) => {
+    if (err) {
+        console.log('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('success');
+});
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE database';
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Database created');
+    });
+});*/
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static('public'));
@@ -11,5 +32,5 @@ app.use(defaultRoute)
 
 
 app.listen(3000, () => {
-    
+
 })
