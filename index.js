5397299038c9e22b9e9b8d0cecc47e9fe4b270c9
Arturs <arturspalamarcuks@inbox.lv>
2022-06-28 20:07:25 +0300

    made new view/ fixxed css bug/ updated forms

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require('express')
 const app = express()
 const session = require('express-session');
 const flash = require('connect-flash');
-const notFound = require('./middleware/notFound');
+//const notFound = require('./middleware/notFound');
 const userRoutes = require('./routes/user')
-const shopRoutes = require('./routes/shop')
-const Logger = require('./middleware/logger')
+//const shopRoutes = require('./routes/shop')
+//const Logger = require('./middleware/logger')
 //const appError = require('./utils/customError')
 
 
@@ -14,6 +14,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static('public'));
 
 
 const secret = process.env.SECRET || 'random';
@@ -22,7 +23,7 @@ const sessionConfig = {
     secret: secret,
     resave: false,
     saveUninitialized: true,
-    cookie:{
+    cookie: {
         expires: Date.now() + 1000 * 60 * 60,
         maxAge: 1000 * 60 * 60,
     }
@@ -33,16 +34,16 @@ app.use(flash())
 
 
 app.use((req, res, next) => {
-  res.locals.success = req.flash('success')
-  res.locals.error = req.flash('error')
-  return next()
+    res.locals.success = req.flash('success')
+    res.locals.error = req.flash('error')
+    return next()
 })
 
 
 
 app.use('/user', userRoutes);
-app.use(shopRoutes)
-app.use(notFound);
+//app.use(shopRoutes)
+//app.use(notFound);
 
 
 
@@ -51,11 +52,11 @@ const start = async () => {
     try {
         const PORT = process.env.PORT || 3000
         app.listen(PORT, () =>
-        console.log(`Server is listening on port ${PORT}...`)
-      )
+            console.log(`Server is listening on port ${PORT}...`)
+        )
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
 };
-  
+
 start();
