1f927437bc93ab00527ef28cf833abe17c7538e5
Arturs <arturspalamarcuks@inbox.lv>
2022-06-28 21:11:16 +0300

    added updateProducts view

--- index.js
+++ index.js
@@ -3,27 +3,15 @@ const express = require('express')
 const app = express()
 const session = require('express-session');
 const flash = require('connect-flash');
-<<<<<<< HEAD
-//const notFound = require('./middleware/notFound');
-const userRoutes = require('./routes/user')
-//const shopRoutes = require('./routes/shop')
-//const Logger = require('./middleware/logger')
-//const appError = require('./utils/customError')
-=======
 const userRoutes = require('./routes/user')
 const defaultRoutes = require('./routes/defaultRoutes')
->>>>>>> 0954687b1e80706ed08885b055cea707284caf8b
 
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static('public'));
-<<<<<<< HEAD
-
-=======
 app.use(express.static('public'))
->>>>>>> 0954687b1e80706ed08885b055cea707284caf8b
 
 const secret = process.env.SECRET || 'random';
 const sessionConfig = {
@@ -49,12 +37,6 @@ app.use((req, res, next) => {
 
 app.use(defaultRoutes)
 app.use('/user', userRoutes);
-<<<<<<< HEAD
-//app.use(shopRoutes)
-//app.use(notFound);
-=======
-
->>>>>>> 0954687b1e80706ed08885b055cea707284caf8b
 
 
 
