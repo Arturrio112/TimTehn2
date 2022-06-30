0aa12a5690d384666b871031e221cd397c19915c
Arturrio112 <arturspalamarcuks@inbox.lv>
2022-06-30 22:30:54 +0300

    deleted useless files

--- middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const expressEror = require('../utils/customError')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof expressEror) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(500).json({ msg: 'Something went wrong, please try again' })
-}
-
-module.exports = errorHandlerMiddleware
